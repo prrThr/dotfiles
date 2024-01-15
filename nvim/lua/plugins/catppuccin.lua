6c5bfe50598307d9f0c1d38d34a65e263627d9be
prrThr <arthur.op9@gmail.com>
2024-01-15 17:56:26 -0300

    transparent background added

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,6 +5,12 @@ return {
   priority = 1000,
   config = function()
     vim.cmd.colorscheme "catppuccin"
+
+    vim.cmd[[ hi Normal guibg=NONE ctermbg=NONE ]]
+    vim.cmd[[ hi SignColumn guibg=NONE ctermbg=NONE ]]
+    vim.cmd[[ hi VertSplit guibg=NONE ctermbg=NONE ]]
+    vim.cmd[[ hi StatusLine guibg=NONE ctermbg=NONE ]]
+    vim.cmd[[ hi StatusLineNC guibg=NONE ctermbg=NONE ]]
+    vim.cmd[[ hi NormalNC guibg=NONE ctermbg=NONE ]]
   end
 }
- 
