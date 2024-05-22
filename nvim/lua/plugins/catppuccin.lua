2729bc7305cf812dca62327b25f3e2e7872c956e
prrThr <arthur.op9@gmail.com>
2024-05-21 22:17:27 -0300

    full theme setup + transparency

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,17 +4,57 @@ return {
   name = "catppuccin", 
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+            light = "latte",
+            dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+            enabled = false, -- dims the background color of inactive window
+            shade = "dark",
+            percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+            comments = { "italic" }, -- Change the style of comments
+            conditionals = { "italic" },
+            loops = {},
+            functions = {},
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+            -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            treesitter = true,
+            notify = false,
+            mini = {
+                enabled = true,
+                indentscope_color = "",
+            },
+            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+    })
+
+    -- setup must be called before loading
     vim.cmd.colorscheme "catppuccin"
-
-    vim.cmd[[ hi Normal guibg=NONE ctermbg=NONE ]]
-    vim.cmd[[ hi SignColumn guibg=NONE ctermbg=NONE ]]
-    vim.cmd[[ hi VertSplit guibg=NONE ctermbg=NONE ]]
-    vim.cmd[[ hi StatusLine guibg=NONE ctermbg=NONE ]]
-    vim.cmd[[ hi StatusLineNC guibg=NONE ctermbg=NONE ]]
-    vim.cmd[[ hi NormalNC guibg=NONE ctermbg=NONE ]]
-
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
-    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
   end
 }
+
