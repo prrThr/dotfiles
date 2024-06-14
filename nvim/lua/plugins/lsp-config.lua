4651ff0874bc7bb2d1c5c504d525324ea128bffd
prrThr <arthur.op9@gmail.com>
2024-06-14 17:08:24 -0300

    new ascii art + go to warning/error

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -34,6 +34,8 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', ']g', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '[g', vim.diagnostic.goto_prev)
     end
   }
 }
